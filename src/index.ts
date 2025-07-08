1c219b5cf8a70b3f040e5427c76c3a7aa924b2f1
Murilo Serpa <murilo.serpa11@gmail.com>
2025-07-07 21:48:05 -0300

    add dotenv configuration to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import dotenv from 'dotenv';
 import type { Request, Response } from "express";
 
+
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
